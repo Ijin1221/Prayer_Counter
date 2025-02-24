9b8f548038884e1625ac33aa238d4dd57b81728c
Ijin George <georgeijin@gmail.com>
2025-02-24 09:40:05 +0530

    count added

---
+++ script.js
@@ -0,0 +1,25 @@
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+
+function handilPrayer1() {
+  let countWindow = document.getElementById('count1')
+  count1++
+  countWindow.textContent = count1;
+}
+function handilPrayer2() {
+  let countWindow = document.getElementById('count2')
+  count2++
+  countWindow.textContent = count2;
+}
+function handilPrayer3() {
+  let countWindow = document.getElementById('count3')
+  count3++
+  countWindow.textContent = count3;
+}
+function handilPrayer4() {
+  let countWindow = document.getElementById('count4')
+  count4++
+  countWindow.textContent = count4;
+}
